0bc46d255cdc049e8f3dc759f42e402aebbaf8de
Andrew Sliwinski <andrew@diy.org>
2014-03-04 16:15:40 -0800

    Remove lodash dep and update package

--- test/governance/lint.js
+++ test/governance/lint.js
@@ -1,1 +1,2 @@
-require('jshint-tap-simple').run(__dirname + '/../../lib/*.js');
+var config = require('fs').readFileSync(__dirname + '/../../.jshintrc');
+require('hint-hint')(__dirname + '/../../lib/index.js', JSON.parse(config.toString()));
